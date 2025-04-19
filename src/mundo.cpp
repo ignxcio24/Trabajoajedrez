2b58e992d69b3dd9ae719470472b9b025ffbf044
JCG-1404 <jorge.crespo@alumnos.upm.es>
2025-04-19 18:32:04 +0200

    Update mundo.cpp
    Declaro el tablero para que se vea en pantalla

--- src/mundo.cpp
+++ src/mundo.cpp
@@ -15,9 +15,10 @@ void Mundo::dibuja()
 	gluLookAt(x_ojo, y_ojo, z_ojo,  // posicion del ojo
 			0.0, 0, 0.0,      // hacia que punto mira  (0,0,0) 
 			0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
-
+	tablero.dibuja();
 	//aqui es donde hay que poner el codigo de dibujo
 	//dibujo del suelo
+	/*
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
 		glColor3ub(255,0,0);
@@ -28,6 +29,7 @@ void Mundo::dibuja()
 		glVertex3d(5.0,0,-5.0);
 	glEnd();
 	glEnable(GL_LIGHTING);
+	*/
 }
 
 void Mundo::mueve()

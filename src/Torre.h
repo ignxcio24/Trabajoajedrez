8fa0029a5d193bf7acee7b21aaa8ac466ec29425
JCG-1404 <jorge.crespo@alumnos.upm.es>
2025-05-11 21:57:02 +0200

    Torre.h
    Modificación en torre.h

--- src/Torre.h
+++ src/Torre.h
@@ -3,11 +3,8 @@
 class Torre:public Pieza
 {
 public:
-    Torre(Vector3D pos, bool blanca);
-
-    void dibuja(float tam_casilla, float offset_x, float offset_y) const override;
-
-    bool movimiento_valido(Vector3D destino) const override;
+	Torre();
+	virtual ~Torre();
 
 };
 

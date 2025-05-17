0b10b798e67a59ba75f2981ac059a7b1f6c58f94
JCG-1404 <jorge.crespo@alumnos.upm.es>
2025-05-17 23:39:37 +0200

    TORRE
    Finalización de Torre.h

--- src/Torre.h
+++ src/Torre.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Pieza.h"
-class Torre:public Pieza
+class Torre :public Pieza
 {
 public:
+	// Constructor y destructor
 	Torre();
 	virtual ~Torre();
+
+	// Métodos, el primero derivación de la clase Pieza
+	virtual void dibujaIndividual() const;
+	bool mueve();
 
 };
 

96f5a56b8a5b734e084ef7cd62c6b6e9437a3ba7
JCG-1404 <jorge.crespo@alumnos.upm.es>
2025-05-25 18:40:27 +0200

    TORRE.CPP
    Push?

--- src/Torre.cpp
+++ src/Torre.cpp
@@ -1,28 +1,14 @@
-#include "Torre.h"
-#include "Tablero.h"
-Torre::Torre(Vector3D pos, bool blanca) : Pieza(pos, blanca) {}
+#include "torre.h"
+#include "freeglut.h"
 
-void Torre::dibuja(float tam_casilla, float offset_x, float offset_y) const {
-    glPushMatrix();
+Torre::Torre() {
+    size = 0.35f;
+}
+Torre::~Torre() {}
 
-    glTranslatef(posicion.x * tam_casilla + tam_casilla / 2 + offset_x,
-        posicion.y * tam_casilla + tam_casilla / 2 + offset_y,
-        0.1f);
-
-    if (es_blanca)
-        glColor3f(1.0f, 1.0f, 1.0f);
-    else
-        glColor3f(0.1f, 0.1f, 0.1f);
-
-    glutSolidCube(0.8f);
-    glPopMatrix();
+bool Torre::mueve() {
+    return true;
 }
 
 
-bool Torre::movimiento_valido(Vector3D destino) const {
-    // Movimiento simple hacia adelante y atras
-    if (es_blanca)
-        return ((destino.x == posicion.x) && (destino.y != posicion.y)) || ((destino.x != posicion.x) && (destino.y == posicion.y));
-    else
-        return ((destino.x == posicion.x) && (destino.y != posicion.y)) || ((destino.x != posicion.x) && (destino.y == posicion.y));
-}
+

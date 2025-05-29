ea8da0e09b2da3930d1a008078f290af0cb88ffa
JCG-1404 <jorge.crespo@alumnos.upm.es>
2025-05-29 02:11:06 +0200

    comentarios
    AY AY AY AY AY AY AY, que me vuelvo loco

--- src/logger.cpp
+++ src/logger.cpp
@@ -2,23 +2,24 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-
+//Inicialización de logger
 static std::ofstream logfile;
 
+//método init
 void logger::init() {
     logfile.open("lastgame.txt");
     if (!logfile.is_open()) {
         std::cerr << "No se pudo abrir lastgame.txt\n";
     }
 }
-
+//método info
 void logger::info(const char* message) {
     std::ostringstream output;
     output << message << std::endl;
     std::cout << output.str();
     if (logfile.is_open()) logfile << output.str();
 }
-
+//método warning
 void logger::shutdown() {
     if (logfile.is_open())
         logfile.close();
